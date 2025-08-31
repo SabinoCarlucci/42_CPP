fcdcb006d03f2554dac8ecc367738ca8492c0987
Sabino Carlucci <scarlucc@c1r1p1.42firenze.it>
2025-08-31 19:59:56 +0200

    CPP01 ex02 done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarlucc <scarlucc@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/31 19:05:30 by scarlucc          #+#    #+#             */
+/*   Updated: 2025/08/31 19:50:04 by scarlucc         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include <string>
+#include <iostream>
+
+int main (void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "And then" << std::endl;
+	
+	std::cout << "value of the string variable: " << str << std::endl;
+	std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+	
+	return (0);
+}
