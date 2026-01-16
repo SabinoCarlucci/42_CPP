2e5c3541777cb5006686e8ecbab548c75d87a1d1
SabinoCarlucci <sabinocarlucci92@gmail.com>
2026-01-16 18:00:03 +0100

    started cpp07 ex01

---
+++ CPP07/ex01/iter.hpp
@@ -0,0 +1,30 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarlucc <scarlucc@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 17:27:51 by scarlucc          #+#    #+#             */
+/*   Updated: 2026/01/16 17:53:53 by scarlucc         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+/* The first parameter is the address of an array 
+	-> pointer ->  T* array
+	
+The second one is the length of the array, passed as a const value
+	-> size_t array_length 
+	
+The third one is a function that will be called on every element of the array
+	*/
+
+template <typename A, typename B, typename C> void iter(T x, T y)
+{
+    
+}
+
+#endif
