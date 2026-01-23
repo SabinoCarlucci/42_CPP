6fd78e5367412b527c5ad062d5e2530268abb89e
SabinoCarlucci <sabinocarlucci92@gmail.com>
2026-01-23 13:20:05 +0100

    cpp08 ex00 done

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,17 +6,21 @@
 /*   By: scarlucc <scarlucc@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 17:57:03 by scarlucc          #+#    #+#             */
-/*   Updated: 2026/01/22 17:22:19 by scarlucc         ###   ########.fr       */
+/*   Updated: 2026/01/23 13:04:31 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
+
 template <typename T>
-void easyfind(T intContainer, int findThis)
+void easyfind(T& intContainer, int findThis)
 {
-	
+	if (std::find(intContainer.begin(), intContainer.end(), findThis) == intContainer.end())
+		throw std::exception();
 }
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,9 +6,61 @@
 /*   By: scarlucc <scarlucc@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 17:23:01 by scarlucc          #+#    #+#             */
-/*   Updated: 2026/01/22 17:23:15 by scarlucc         ###   ########.fr       */
+/*   Updated: 2026/01/23 13:19:41 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "easyfind.hpp"
+#include <exception> //try and catch
+#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+#include <deque>
 
+int main(void)
+{
+	try
+	{
+		std::vector<int> vect;
+		vect.push_back(1);
+		vect.push_back(2);
+		vect.push_back(42);
+
+		easyfind(vect, 42);
+		std::cout << "vect Value found :)" << std::endl;
+
+		
+		std::set<int> set;
+		set.insert(1);
+		set.insert(2);
+		set.insert(33);
+
+		easyfind(set, 2);
+		std::cout << "set Value found :)" << std::endl;
+
+
+		std::list<int> lst;
+		lst.push_back(1);
+		lst.push_back(22);
+		lst.push_back(33);
+
+		easyfind(lst, 22);
+		std::cout << "lst Value found :)" << std::endl;
+
+
+		std::deque<int> dq;
+		dq.push_back(11);
+		dq.push_back(22);
+		dq.push_back(33);
+
+		easyfind(dq, 11);
+		std::cout << "dq Value found :)" << std::endl;
+		
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Value not found :(" << '\n';
+	}
+	
+}
