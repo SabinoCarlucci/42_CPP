24ec885c39445334161855b5c84d1469e87b750c
SabinoCarlucci <sabinocarlucci92@gmail.com>
2026-01-27 17:14:03 +0100

    almost done cpp08

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: scarlucc <scarlucc@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 16:00:30 by scarlucc          #+#    #+#             */
-/*   Updated: 2026/01/26 18:17:23 by scarlucc         ###   ########.fr       */
+/*   Updated: 2026/01/27 16:59:28 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -23,10 +23,32 @@ class MutantStack : public std::stack<T>
 	//canonical form
 	MutantStack() {}
 	~MutantStack() {}
-	MutantStack(const MutantStack& other) {}
+	MutantStack(const MutantStack& other) : std::stack<T>(other) {}
 	MutantStack &operator=(const MutantStack &other)
+	{
+		std::stack<T>::operator=(other);
+		return *this;
+	}
 	
-		
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+	
+
+	iterator begin() {return this->c.begin();}
+	iterator end() {return this->c.end();}
+
+	const_iterator begin() const {return this->c.begin();}
+	const_iterator end() const {return this->c.end();}
+
+	reverse_iterator rbegin() {return this->c.rbegin();}
+	reverse_iterator rend() {return this->c.rend();}
+
+	const_reverse_iterator rbegin() const {return this->c.rbegin();}
+	const_reverse_iterator rend() const {return this->c.rend();}
+
+	//c e' un membro dipendente del template base
 };
 
 #endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,8 +6,60 @@
 /*   By: scarlucc <scarlucc@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 15:59:59 by scarlucc          #+#    #+#             */
-/*   Updated: 2026/01/26 16:00:19 by scarlucc         ###   ########.fr       */
+/*   Updated: 2026/01/27 17:05:23 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "mstack.top: " << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << "mstack.size: " << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	std::cout << std::endl;
+	std::cout << "print all MutantStack:" << std::endl;
+	
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	std::cout << "end MutantStack:" << std::endl;
+	std::cout << std::endl;
+
+	std::stack<int> s(mstack);
+
+	// confronto con list
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	for (std::list<int>::iterator lit = lst.begin(); lit != lst.end(); ++lit)
+		std::cout << *lit << std::endl;
+
+	return 0;
+}
